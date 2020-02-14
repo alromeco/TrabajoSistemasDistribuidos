00de4eed1867e1b5232776f2d62eca4aa3946e66
frolavar <frolavar@CCTL138-17.unirioja.loc>
2020-02-14 11:53:02 +0100

    Haciendo mesa

--- src/p1/Mesa.java
+++ src/p1/Mesa.java
@@ -1,5 +1,19 @@
 package p1;
 
+import java.util.Deque;
+
 public class Mesa {
-	
+	private Deque<Carta> Bastos;
+	private Deque<Carta> Espadas;
+	private Deque<Carta> Copas;
+	private Deque<Carta> Oros;
+	public Mesa() {
+		this.Bastos=null;
+		this.Copas=null;
+		this.Espadas=null;
+		this.Oros=null;
+	}
+	public void anadir(Carta c) {
+		
+	}
 }
